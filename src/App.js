32416e6cb079f9008b33431bea9a4b7a43d9777c
Travis Carter <carter.travis94@gmail.com>
2019-09-26 10:10:56 -0600

    change color

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
     return (
       <Container>
         <Segment textAlign="center">
-          <Header as="h3" textAlign="center">Todo List</Header>
+          <Header color="blue" as="h3" textAlign="center">Todo List</Header>
           <Form onSubmit={this.handleSubmit}>
             <Input 
               required
